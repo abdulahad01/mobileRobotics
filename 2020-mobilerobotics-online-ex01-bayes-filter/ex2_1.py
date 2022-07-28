1eeaae9874e41cae9339a9d4199836f85fd162ba
abdulahad01 <abdudeturk@gmail.com>
2022-07-28 20:49:21 +0530

    Modified bayes filter

--- 2020-mobilerobotics-online-ex01-bayes-filter/ex2_1.py
+++ 2020-mobilerobotics-online-ex01-bayes-filter/ex2_1.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Author : Adbdul Ahad
+# Author : Abdul Ahad
 # Repo : https://github.com/abdulahad01/mobile_sensing_robotics/
 
 def plot_belief(belief):
@@ -29,56 +29,34 @@ def plot_belief(belief):
 def motion_model(distr,command) :
     
     pCorrect = 0.7
-    pUndershoot = 0.1
+    pOpp = 0.1
     pStay = 0.2
-    new_distr = np.zeros(len(distr))
     
     # if forward command, control input ==1 else -1
     if command == 'F':
         U = 1
-    else :
-        U = -1
         # the probability that the robot is in its current position is the total probability of it moving correctly from
         # the previous cell and the probablity it has undershot or stays in its prev position
         # the world is not cyclic so we account for the edge cases i.e i=0 and i =14
-    for i in range (len(distr)):
-        if i >0 and i <14:
-            new_distr[i] = distr[(i-U)%len(distr)]*pCorrect + distr[(i+U)%len(distr)]*pUndershoot + distr[(i)%len(distr)]*pStay
-        elif i == 0:
-            new_distr[i] = distr[(i+U)%len(distr)]*pUndershoot + distr[(i)%len(distr)]*pStay
-        else :
-            new_distr[i] = distr[(i-U)%len(distr)]*pCorrect + distr[(i)%len(distr)]*pStay
-    return new_distr
+        for i in range (len(distr)):
+            if i >0 and i <14:
+                distr[i] = distr[(i+U)%len(distr)]*pCorrect + distr[(i-U)%len(distr)]*pOpp + distr[i]*pStay
+            elif i == 14:
+                distr[i] = distr[(i-U)%len(distr)]*pOpp + distr[i]*pStay
+            else :
+                distr[i] = distr[(i+U)%len(distr)]*pCorrect + distr[i]*pStay
+    else :
+        U = -1
+        for i in range (len(distr)):
+            if i >0 and i <14:
+                distr[i] = distr[(i+U)%len(distr)]*pCorrect + distr[(i-U)%len(distr)]*pOpp + distr[i]*pStay
+            elif i == 1:
+                distr[i] = distr[(i-U)%len(distr)]*pOpp + distr[i]*pStay
+            else :
+                distr[i] = distr[(i+U)%len(distr)]*pCorrect + distr[i]*pStay
+
+    return distr/sum(distr)
                 
-
-# def sensor_model(observation, belief, world):
-#     p_white = 0.7
-#     p_black = 0.9
-#     belief_out = np.copy(belief)
-
-#     if observation == 0:
-
-#         for i, val in enumerate(world):
-#             # Assuming black observation by sensor and world is black too
-#             if val == 0:
-#                 belief_out[i] = p_black * belief[i]
-
-#             # Accounting for sensor noise in the sensor that the world is white and sensor gives black
-#             else:
-#                 belief_out[i] = (1 - p_white) * belief[i]
-
-#     else:
-#         for i, val in enumerate(world):
-#             # Assuming white observation by sensor and world is white too
-#             if val == 1:
-#                 belief_out[i] = p_white * belief[i]
-
-#             # Accounting for sensor noise in the sensor that the world is black and sensor gives white
-#             else:
-#                 belief_out[i] = (1 - p_black) * belief[i]
-
-#     # Normalizing it to get the PDF so sum of all elements must be one
-#     return belief_out / sum(belief_out)
 
 def sensor_model(observation, belief, world):
     pWhite  = 0.7   #propability that the sensor correctly observes the cell is white  p(white|cell is white)
