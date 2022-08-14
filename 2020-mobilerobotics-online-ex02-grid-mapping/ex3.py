f09709bf87cec9703184f171b9b6a73f7ba61401
abdulahad01 <abdudeturk@gmail.com>
2022-08-14 18:14:42 +0530

    updated motion model

--- 2020-mobilerobotics-online-ex02-grid-mapping/ex3.py
+++ 2020-mobilerobotics-online-ex02-grid-mapping/ex3.py
@@ -16,8 +16,8 @@ def init_gridmap(size, res):
 def world2map(pose, gridmap, map_res):
     origin = np.array(gridmap.shape)/2
     new_pose = np.zeros(pose.shape)
-    new_pose[0:] = np.round(pose[0:]/map_res) + origin[0];
-    new_pose[1:] = np.round(pose[1:]/map_res) + origin[1];
+    new_pose[0:] = np.round(pose[0:]/map_res) + origin[0]
+    new_pose[1:] = np.round(pose[1:]/map_res) + origin[1]
     return new_pose.astype(int)
 
 def v2t(pose):
--- 2020-mobilerobotics-online-ex03-motion-model/ex4.py
+++ 2020-mobilerobotics-online-ex03-motion-model/ex4.py
@@ -2,10 +2,13 @@
 
 import numpy as np
 import math
-import random
 import matplotlib.pyplot as plt
 
 #to convert cordinates in the gridmap to world cordinates
+def plot_map(gridmap):
+    plt.figure()
+    plt.imshow(gridmap, cmap='Greys')
+
 def map2world(grid,i,j,x_0):
     origin = [grid.shape[0]/2 , grid.shape[1]/2]
     pose = [0,0,0]
@@ -14,21 +17,21 @@ def map2world(grid,i,j,x_0):
     return pose
 
 def inverse_motion_model(u):
-    del_rot1 = math.atan2((u[1][1]-u[0][1]),(u[1][0]-u[0][0])) - u[0][2]
-    del_trans = math.sqrt( (u[1][0]-u[0][0])**2 + (u[1][1]-u[0][1])**2)
-    del_rot2 = u[1][2]-u[0][2]-del_rot1
-    return del_rot1, del_trans, del_rot2
+    rot1 = math.atan2((u[1][1]-u[0][1]),(u[1][0]-u[0][0])) - u[0][2]
+    trans = math.sqrt( (u[1][0]-u[0][0])**2 + (u[1][1]-u[0][1])**2)
+    rot2 = u[1][2]-u[0][2]-rot1
+    return rot1, trans, rot2
 
     
 def prob(query, std):
       return max(0,(1/(math.sqrt(6)*std) - (abs(query)/(6 * (std**2)))))
 
-def motion_model_odometry(x0,xt,u_t,alpha,gridmap):
-    del_rot1, del_trans, del_rot2 = inverse_motion_model(u_t)
-    del_rot1_hat, del_trans_hat, del_rot2_hat = inverse_motion_model([x0,xt])
-    p1 = prob((del_rot1-del_rot1_hat),(alpha[0]*abs(del_rot1)+alpha[1]*del_trans))
-    p2 = prob((del_trans-del_trans_hat),(alpha[2]*del_trans+alpha[3]*abs(del_rot1+del_rot2)))
-    p3 = 1
+def motion_model_odometry(x0,xt,u_t,alpha):
+    rot1, trans, rot2 = inverse_motion_model(u_t)
+    rot1_hat, trans_hat, rot2_hat = inverse_motion_model([x0,xt])
+    p1 = prob((rot1-rot1_hat),(alpha[0]*abs(rot1)+alpha[1]*trans))
+    p2 = prob((trans-trans_hat),(alpha[2]*trans + alpha[3]*(abs(rot1)+abs(rot2))))
+    p3 = prob((rot2 - rot2_hat),(alpha[0]*rot2 + alpha[1]*trans))
     return p1*p2*p3
 
 def sample(b):
@@ -37,13 +40,13 @@ def sample(b):
 
 def sample_motion_model_odometry(x0,u_t,alpha):
     
-        del_rot1, del_trans, del_rot2 = inverse_motion_model(u_t)
-        del_rot1_hat = del_rot1 +sample( alpha[0]*abs(del_rot1) + alpha[1]*del_trans)
-        del_trans_hat = del_trans +sample( alpha[2]*del_trans + alpha[3]* (abs(del_rot1)+abs(del_rot2)) ) 
-        del_rot2_hat = del_rot2 +sample( alpha[0]*abs(del_rot2)+ alpha[1]*del_trans)
+        rot1, trans, rot2 = inverse_motion_model(u_t)
+        rot1_hat = rot1 + sample( alpha[0]*abs(rot1) + alpha[1]*trans)
+        trans_hat = trans + sample( alpha[2]*trans + alpha[3]* (abs(rot1)+abs(rot2)) ) 
+        rot2_hat = rot2 + sample( alpha[0]*abs(rot2)+ alpha[1]*trans)
         
-        x = x0[0] + del_trans_hat*math.cos(x0[2]+del_rot1_hat)
-        y = x0[1] + del_trans_hat*math.sin(x0[2]+del_rot1_hat)
-        theta = x0[2] + del_rot1_hat + del_rot2_hat
+        x = x0[0] + trans_hat*math.cos(x0[2]+rot1_hat)
+        y = x0[1] + trans_hat*math.sin(x0[2]+rot1_hat)
+        theta = x0[2] + rot1_hat + rot2_hat
         return [x, y, theta]
 
