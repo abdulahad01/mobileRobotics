2f9979d2f6ee795df0fbea9e8b5e23ac67e7120a
abdulahad01 <abdudeturk@gmail.com>
2022-10-22 11:46:07 +0530

    observation model final commit

--- Observation model/ex5.py
+++ Observation model/ex5.py
@@ -1,10 +1,16 @@
-# github.com/abdulahad01
+"""
+Completed by Abdul Ahad
+https://github.com/abdulahad01/mobileRobotics
+"""
 from math import *
 import matplotlib.pyplot as plt
-def landmark_observation_model(z,x,m,sigma):
+
+
+def landmark_observation_model(z, x, m, sigma):
     z_exp = sqrt((x[0]-m[0])**2 + (x[1]-m[1])**2)
     prob = exp((-0.5*(z-z_exp)**2)/sigma**2)*1/sqrt(2*pi*sigma**2)
     return prob
+
 
 def plot_map(gridmap):
     plt.figure()
